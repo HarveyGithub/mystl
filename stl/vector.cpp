afa1245360bc0e331bf1ad0b176940a76bc9fd5d
gongruixin2012 <18988581734@163.com>
2025-01-21 17:21:57 +0800

    11

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -1,48 +1,73 @@
-#include<iostream>
-#include<cassert>
+#include<bits/stdc++.h>
 using namespace std;
-template<typename T>
-class Vector{
-private:
-    size_t Size,Able;
-    T* Data;
-public:
-    bool Out_Assert;
-    Vector(){
-        Size=0;
-        Able=1;
-        Data=new T[1];
-        Out_Assert=true;
-    }
-    void push_back(T value){
-        if(Size==Able){
-            T* NewData=new T[Able*2];
-            for(size_t i=0;i<Size;i++){
-                NewData[i]=Data[i];
-            }
-            delete[] Data;
-            Data=NewData;
-            Able*=2;
-        }
-        Data[Size++]=value;
-    }
-    void pop_back(){
-        if(Size==0) return;
-        Size--;
-    }
-    size_t size(){
-        return Size;
-    }
-    T operator[](const size_t &index){
-        if(index>=Size){
-            if(Out_Assert)assert(0);
-            else return 0;
-        }
-        return Data[index];
-    }
-    T at(const size_t &index){
-        if(index>=Size) return "No";
-        return Data[index];
-    }
-};
-Vector<int> v;
+#define Tie ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define int long long
+int T,n,m,t;
+int year[146097];
+int mouth[146097];
+int day[146097];
+int pd(int year,int mouth)
+{
+	if(mouth==1)
+		return 31;
+	if(mouth==2)
+		if(year%4==0&&year%100!=0||year%400==0)
+			return 29;
+	else
+		return 28;
+	if(mouth==3)
+		return 31;
+	if(mouth==4)
+		return 30;
+	if(mouth==5)
+		return 31;
+	if(mouth==6)
+		return 30;
+	if(mouth==7)
+		return 31;
+	if(mouth==8)
+		return 31;
+	if(mouth==9)
+		return 30;
+	if(mouth==10)
+		return 31;
+	if(mouth==11)
+		return 30;
+	if(mouth==12)
+		return 31;
+}
+signed main()
+{
+	Tie;
+	mouth[0]=day[0]=1;
+	for(int i=1;i<146097;i++)
+	{
+		day[i]=day[i-1]+1;
+		mouth[i]=mouth[i-1];
+		year[i]=year[i-1];
+		if(day[i]>pd(year[i],mouth[i]))
+			mouth[i]++,day[i]=1;
+		if(mouth[i]>12)
+			year[i]++,mouth[i]=1;
+	}
+	cin>>T;
+	while(T--)
+	{
+		cin>>n;
+		if(n>2299160)
+		{
+			n-=2159351;
+			t=n/146097*400+1200;
+			n%=146097;
+		}
+		else
+		{
+			t=n/1461*4-4712;
+			n%=1461;
+		}
+		if(t+year[n]>0)
+			cout<<day[n]<<' '<<mouth[n]<<' '<<t+year[n]<<'\n';
+		else
+			cout<<day[n]<<' '<<mouth[n]<<' '<<1-t-year[n]<<" BC\n"; 
+	}
+}
